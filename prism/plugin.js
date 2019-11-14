297e92ef50c249085d9efeea6e89027894c9eab3
Arno Fiva <arnofiva@gmail.com>
2019-11-14 14:47:22 +0100

    Deploy latest version

--- prism/plugin.js
+++ prism/plugin.js
@@ -10,29 +10,29 @@
         Prism.highlightElement(code);
     }
   }
-  
+
   function injectPrism() {
     if (!document.getElementById('prismCSS')) {
       var link = document.createElement('link');
       link.id = 'prismCSS';
       link.rel = 'stylesheet';
-      link.href = '../prism/prism.css';
+      link.href = './prism/prism.css';
       document.head.appendChild(link);
     }
-  
+
     if (!document.getElementById('prismJS')) {
       var script = document.createElement('script');
       script.id = 'primsJS';
-      script.src = '../prism/prism.js';
+      script.src = './prism/prism.js';
       document.head.appendChild(script);
-    
+
       script.onload = function onPrismLoad() {
         initPrism();
       };
     }
   }
-  
-  
+
+
   if (Reveal.isReady()) {
     injectPrism();
   }
